813e5eb935a8f47af2068fdc716f0cefdf33d894
zhangrui-wolf <zhangrui_wolf@163.com>
2021-08-11 12:18:51 +0800

    Fix bug in SEPlugin

--- RepVGG/plugins/se_plugin.py
+++ RepVGG/plugins/se_plugin.py
@@ -5,5 +5,5 @@ from mmcv.cnn import PLUGIN_LAYERS
 @PLUGIN_LAYERS.register_module()
 class SEPlugin(SELayer):
 
-    def __init__(self, **kwargs):
-        super(SEPlugin, self).__init__(**kwargs)
+    def __init__(self, in_channels, **kwargs):
+        super(SEPlugin, self).__init__(channels=in_channels, **kwargs)
